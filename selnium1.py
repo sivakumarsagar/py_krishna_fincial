6ce42ec2eee858841e5c086fcb1d7c6b21a05e0c
sivakumarsagar <52564643+sivakumarsagar@users.noreply.github.com>
2020-03-05 03:51:47 -0500

    Update selnium1.py

--- selnium1.py
+++ selnium1.py
@@ -1,6 +1,7 @@
 #from selenium import webdriver
 #driver= webdriver.Chrome()
 from selenium import webdriver
+driver = webdriver.Chrome(executable_path= r'C:\Python38\geckodriver.exe')
 browser = webdriver.Firefox()
 #browser.get('http://inventwithpython.com')
 driver.get("https://localhost:5000")
