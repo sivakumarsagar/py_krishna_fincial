8f6933928f7b5c74509fea6d7dc7300a2e690859
sivakumarsagar <52564643+sivakumarsagar@users.noreply.github.com>
2020-03-05 21:55:25 -0500

    Update selnium1.py

--- selnium1.py
+++ selnium1.py
@@ -4,5 +4,5 @@ from selenium import webdriver
 driver = webdriver.Chrome(r'C:\Python38\chromedriver.exe')
 #browser = webdriver.Firefox()
 #browser.get('http://inventwithpython.com')
-driver.get("https://github.com/sivakumarsagae")
+driver.get("https://github.com/sivakumarsagar")
 driver.implicitly_wait(3000)
