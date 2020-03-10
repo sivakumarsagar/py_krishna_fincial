f884396abe0395248811d514fefc693631858026
sivakumarsagar <52564643+sivakumarsagar@users.noreply.github.com>
2020-03-10 05:29:57 -0400

    Update selnium1.py

--- selnium1.py
+++ selnium1.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome(r'C:\Python38\chromedriver.exe')
