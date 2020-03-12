3a3cc1d69f9eb47e61bc8c291090adad764d5848
sivakumarsagar <52564643+sivakumarsagar@users.noreply.github.com>
2020-03-12 02:53:05 -0400

    Update selnium1.py

--- selnium1.py
+++ selnium1.py
@@ -1,8 +1,8 @@
 import sqlite3
 
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
